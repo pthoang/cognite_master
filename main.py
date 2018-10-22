cecd2d29a35b1006a2c73729bc2a5176f6f58267
Phi Thien Hoang <Phi Thien Hoang>
2018-10-22 19:44:18 +0200

    add plot input and control tags

--- main.py
+++ main.py
@@ -65,7 +65,6 @@ def run_linear_regression(data, test_size, dates):
     # predicted_temp = pd.DataFrame({'Predicted Temp': lr_pred_temp})
     # predicted_temp.set_index(dates, inplace=True)
     # predicted_temp['Predicted Temp'].plot(label='Predicted temperature', c='r', linestyle='--')
-    print('lol')
     plotting.plt_act_pred(lr_act_temp.to_frame(), pd.DataFrame({'Predicted': lr_pred_temp}), dates, 'Temperature')
 
     # lr_act_press_df = lr_act_press.to_frame()
@@ -77,43 +76,74 @@ def run_linear_regression(data, test_size, dates):
     plotting.plt_act_pred(lr_act_press.to_frame(), pd.DataFrame({'Predicted': lr_pred_press}), dates ,'Pressure')
 
 
+def plot_input_control(data):
+    fig1, axs1 = plt.subplots(3, 1, figsize=(10, 10))
+    fig1.subplots_adjust(hspace=0.3)
+    fig2, axs2 = plt.subplots(3, 1, figsize=(10, 10))
+    fig2.legend().set_visible(False)
+    fig2.subplots_adjust(hspace=0.3)
+
+    axs1[0].plot(data[constants.COMPRESSOR_SUCTION_TEMPERATURE + '|average'], label='_nolegend_')
+    axs1[0].set_title('Input temperature degC')
+
+    axs1[1].plot(data[constants.COMPRESSOR_SUCTION_PRESSURE + '|average'], label='_nolegend_')
+    axs1[1].set_title('Input pressure barg')
+
+    axs1[2].plot(data[constants.COMPRESSOR_GAS_INFLOW + '|average'], label='_nolegend_')
+    axs1[2].set_title('Gas inflow')
+
+    axs2[0].plot(data[constants.ANTI_SURGE_VALVE_POSITION + '|average'].interpolate(), label='_nolegend_')
+    axs2[0].set_title('Anti-surge valve position %')
+
+    axs2[1].plot(data[constants.SUCTION_THROTTLE_VALVE_POSITION + '|average'].interpolate(), label='_nolegend_')
+    axs2[1].set_title('Suction throttle valve position %')
+
+    axs2[2].plot(data[constants.COMPRESSOR_SHAFT_POWER + '|average'].interpolate(), label='_nolegend_')
+    axs2[2].set_title('Shaft power kW')
 
 def main():
-    start = datetime.datetime(2000,1,1)
-    end = '1w-ago'
-    granularity = '12h'
-    aggregates = ['avg', 'min', 'max']
+    start = datetime.datetime(2013,9,1)
+    end = datetime.datetime(2018,9,7)
+    granularity = '1h'
+    aggregates = [
+        'avg',
+        'min',
+        'max'
+    ]
     tags = [
         constants.COMPRESSOR_SUCTION_PRESSURE,
         constants.COMPRESSOR_SUCTION_TEMPERATURE,
         constants.COMPRESSOR_DISCHARGE_TEMPERATURE,
         constants.COMPRESSOR_GAS_INFLOW,
         constants.COMPRESSOR_DISCHARGE_PRESSURE,
+        constants.ANTI_SURGE_VALVE_POSITION,
+        constants.SUCTION_THROTTLE_VALVE_POSITION,
+        constants.COMPRESSOR_SHAFT_POWER,
     ]
     test_size = 30
     data  = get_datapoints_frame(time_series=tags, start=start, end=end, granularity=granularity, aggregates=aggregates)
+    data.set_index(pd.to_datetime(data['timestamp'], unit='ms'), inplace=True)
 
-    data[constants.COMPRESSOR_DISCHARGE_TEMPERATURE + '|average'].plot(label='Input temperature')
-    data[constants.COMPRESSOR_SUCTION_TEMPERATURE+'|average'].plot()
-    data[constants.COMPRESSOR_SUCTION_PRESSURE+ '|average'].plot()
-    data[constants.COMPRESSOR_DISCHARGE_PRESSURE+'|average'].plot()
+
+    plot_input_control(data)
+
 
     dates = pd.to_datetime(data['timestamp'][-test_size:])
 
     # run_linear_regression(data, test_size, dates)
 
-    lstm_act_temp, lstm_pred_temp = lstm.predict(data[
-                                                     [
-                                                        constants.COMPRESSOR_SUCTION_PRESSURE + '|average',
-                                                        constants.COMPRESSOR_SUCTION_TEMPERATURE + '|average',
-                                                        constants.COMPRESSOR_GAS_INFLOW + '|average'
-                                                     ]
-                                                 ].interpolate(),
-                                                 data[constants.COMPRESSOR_DISCHARGE_TEMPERATURE + '|average'].interpolate(),
-                                                 test_size=test_size)
-
-
-    plotting.plt_act_pred(pd.DataFrame({'Actual': lstm_act_temp}), pd.DataFrame({'Predicted': lstm_pred_temp}), dates, 'Temperature')
+    # lstm_act_temp, lstm_pred_temp = lstm.predict(data[
+    #                                                  [
+    #                                                     constants.COMPRESSOR_SUCTION_PRESSURE + '|average',
+    #                                                     constants.COMPRESSOR_SUCTION_TEMPERATURE + '|average',
+    #                                                     constants.COMPRESSOR_GAS_INFLOW + '|average'
+    #                                                  ]
+    #                                              ].interpolate(),
+    #                                              data[constants.COMPRESSOR_DISCHARGE_TEMPERATURE + '|average'].interpolate(),
+    #                                              test_size=test_size)
+    #
+    #
+    # plotting.plt_act_pred(pd.DataFrame({'Actual': lstm_act_temp}), pd.DataFrame({'Predicted': lstm_pred_temp}), dates, 'Temperature')
 
 
 
